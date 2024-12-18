00e6a2aa31d33bfe6c085ff4b91f604f37aeeb9a
froemailtest <froemailtest@gmail.com>
2024-12-18 10:16:33 +0530

    first commit

--- CV.com/src/component/pages/Home.jsx
+++ CV.com/src/component/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Button } from '@mui/material';
 // import MenuCard from '../uiComponent/MenuCard'
-// import logo from '../../../src/free-resume-sample-first-screen-image.webp'
+import logo from '../../../src/free-resume-sample-first-screen-image.webp'
 // import logo from ''
 // import { useCookies } from 'react-cookie';
 import '../styledComponent/home.css'
+import { useNavigate  } from 'react-router-dom';
 
 function Home() {
   
-
+  const navigate = useNavigate();
 
   return (
     <React.Fragment>
@@ -20,7 +21,7 @@ function Home() {
             <div className='Heading-2'>Create your resume easily with our free builder and professional templates with Artificial Intelligence</div>
             <div className='button-div'>
               <div className='button-1-div'>
-              <Button variant="contained" className='button-1'  >Create My Resume</Button>
+              <Button variant="contained" className='button-1' onClick={()=>navigate('/dashboard')}  >Create My Resume</Button>
               </div>
               <div className='button-2-div'>
               <Button variant="outlined" className='button-2'>Sample Resume</Button>
@@ -30,7 +31,7 @@ function Home() {
         </div>
       <img
         className='firstImage'
-        // src={logo}
+        src={logo}
         alt="Example Image"
       />
     </div>
